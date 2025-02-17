811cc69688074202c05200434b588d2fdabfe5ad
rzanei <dev.rzanei@gmail.com>
2025-02-17 12:33:50 +0000

    cmd: cargo new --lib light-writer-rs

---
+++ light-writer-rs/target/debug/deps/light_writer_rs-73390052647ec374.d
@@ -0,0 +1,5 @@
+/home/rzanei/dev/the-archive/light-writer-rs/target/debug/deps/liblight_writer_rs-73390052647ec374.rmeta: src/lib.rs
+
+/home/rzanei/dev/the-archive/light-writer-rs/target/debug/deps/light_writer_rs-73390052647ec374.d: src/lib.rs
+
+src/lib.rs:
